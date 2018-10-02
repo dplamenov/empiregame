28fd82b1f627551472b9ab1950b517e978aa0684
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2018-10-02 17:55:03 +0300

    working on autosetup
    working on autosetup
    make desing on page

--- project/setup/index.php
+++ project/setup/index.php
@@ -1,8 +1,24 @@
 <?php
-/**
- * Project: game.
- * File: index.php
- * Developer: dplamenov@icloud.com
- * Date: 1.10.2018 г.
- * Time: 16:44
- */
+?>
+<!doctype html>
+<html>
+    <head>
+        <title>Setup</title>
+    </head>
+    <body>
+        <div id="container">
+            <h1>Setup</h1>
+            <h2>Database:</h2>
+            <form>
+                <label>Database host:</label>
+                <input name="database_host" value="127.0.0.1"/>
+                <label>Username:</label>
+                <input name="username" value="root"/>
+                <label>Password:</label>
+                <input name="password" value="password"/>
+                <label>Database name:</label>
+                <input name="database_name" value="game"/>
+            </form>
+        </div>
+    </body>
+</html>
