940124eb75c5c0b8990aaa006fb85f2b4b7a74c3
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2018-10-04 15:05:50 +0300

    get data from post
    get data from post

--- project/setup/index.php
+++ project/setup/index.php
@@ -1,4 +1,13 @@
 <?php
+if(isset($_POST['database_host']) && isset($_POST['username']) && isset($_POST['password'])
+&& isset($_POST['database_name'])){
+
+    $database_host = trim($_POST['database_host']);
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+    $database_name = trim($_POST['database_name']);
+
+}
 ?>
 <!doctype html>
 <html>
@@ -11,16 +20,16 @@
             <h2>Database:</h2>
             <form action="index.php" method="post">
                 <label>Database host:</label>
-                <input name="database_host" value="127.0.0.1"/>
+                <input name="database_host" placeholder="127.0.0.1"/>
                 <br />
                 <label>Username:</label>
-                <input name="username" value="root"/>
+                <input name="username" placeholder="root"/>
                 <br />
                 <label>Password:</label>
-                <input name="password" value="password"/>
+                <input name="password" placeholder="password"/>
                 <br />
                 <label>Database name:</label>
-                <input name="database_name" value="game"/>
+                <input name="database_name" placeholder="game"/>
 
                 <button type="submit">Next</button>
             </form>
