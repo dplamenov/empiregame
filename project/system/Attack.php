eb63d805f37391ec9f60637368e9c8e566373cf7
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-01-08 16:04:43 +0200

    work on Attack class not finished

--- project/system/Attack.php
+++ project/system/Attack.php
@@ -14,6 +14,11 @@ class Attack
             echo '<pre>' . print_r($user1_id_army_r, true) . '</pre>';
         }
 
+        $user2_id_army = mysqli_query($dbc,"SELECT * FROM `user_army` WHERE `user_id` = " . $user2_id);
+        while($user2_id_army_r = mysqli_fetch_assoc($user2_id_army)){
+            echo '<pre>' . print_r($user2_id_army_r, true) . '</pre>';
+        }
+
 
     }
 }
