0020032275d22407c901816148ea9d3120563c19
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-04-14 16:51:25 +0300

    translate to english

--- project/checkbuild.php
+++ project/checkbuild.php
@@ -6,13 +6,13 @@ $get_build_q = mysqli_query($dbc, $getbuild_sql);
 echo '<p>Your money: $' . userdata($_SESSION['user']['user_id'], 'money', $dbc) . '</p>';
 
 echo '<p>Town hall</p>';
-echo '<table border="1"><tr><td>Name</td><td>Money</td><td>Време</td><td>Построй</td></tr>';
+echo '<table border="1"><tr><td>Name</td><td>Money</td><td>Time</td><td>Build</td></tr>';
 while ($building = mysqli_fetch_assoc($get_build_q)) {
     if (userdata($_SESSION['user']['user_id'], 'money', $dbc) >= $building['money']) {
         $link = '<a href="?build=' . $building['building_id'] . '">Build</a>';
         $class = 'ok';
     } else {
-        $link = 'Нямаш пари';
+        $link = 'No money';
     }
     echo '<tr><td>' . ucfirst($building['build_name']) . '</td><td>$' . $building['money'] . '</td><td>' . $building['time'] . ' Min</td><td>' . $link . @$link2 . '</td></tr>';
 }
