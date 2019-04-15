b2a62ff4e9538460302b515297ed46cedd797e0a
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-04-15 15:21:11 +0300

    update user last login

--- project/login.php
+++ project/login.php
@@ -6,10 +6,16 @@ $user_pass = $_POST['pass'];
 $users = mysqli_query($dbc, "SELECT * FROM users WHERE pass='" . $user_pass . "' AND user_name='" . $user_name . "' AND active = 1");
 
 $row = mysqli_fetch_assoc($users);
+$last_login = $row['lastlogin'];
 $_SESSION['user']['user_name'] = $row['user_name'];
 $_SESSION['user']['user_id'] = $row['user_id'];
 $_SESSION['user']['xp'] = $row['xp'];
 $_SESSION['user']['money'] = $row['money'];
+
+$lastlogin = $row['lastlogin'];
+$days = abs(time() - $lastlogin) / 60 / 60 / 24;
+mysqli_query($dbc, "UPDATE `users` SET `lastlogin`=  " . time(). " WHERE `user_id` = " . $row['user_id']);
+mysqli_query($dbc, "UPDATE `users` SET `money`=  " . $days * 500 . " WHERE `user_id` = " . $row['user_id']);
 if (mysqli_num_rows($users) == 1) {
     $_SESSION['islogged'] = true;
     echo 'window.location.reload(true); ';
