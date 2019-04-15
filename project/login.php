7ae5673974b6a30dc496adf587c692fcd9035a69
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-04-15 15:30:27 +0300

    bug fix

--- project/login.php
+++ project/login.php
@@ -15,7 +15,7 @@ $lastlogin = $row['lastlogin'];
 $hours = abs(time() - $lastlogin) / 60 / 60;
 mysqli_query($dbc, "UPDATE `users` SET `lastlogin`=  " . time(). " WHERE `user_id` = " . $row['user_id']);
 if($hours >= 1){
-    mysqli_query($dbc, "UPDATE `users` SET `money`=  " . $hours * 50 . " WHERE `user_id` = " . $row['user_id']);
+    mysqli_query($dbc, "UPDATE `users` SET `money`=  `money` + " . $hours * 50 . " WHERE `user_id` = " . $row['user_id']);
 }
 if (mysqli_num_rows($users) == 1) {
     $_SESSION['islogged'] = true;
