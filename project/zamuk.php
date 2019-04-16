cd3f0e6824c7befb87298619accb0d327588a762
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-04-16 15:32:26 +0300

    translate & bug fix

--- project/zamuk.php
+++ project/zamuk.php
@@ -2,5 +2,11 @@
 session_start();
 include 'config.php';
 //echo '<p>Вашите пари: ' . userdata($_SESSION['user']['user_id'], 'money',$dbc) . 'лева</p>';
-echo '<p style="margin-left: 40%">Замък</p>';
-echo '<button type="button" class="btn btn-danger" style="margin-left: 5%"><a href="?find_opponent=1">Намери противник</a></button>';
+echo '<p style="margin-left: 40%">Castle</p>';
+echo '<button type="button" class="btn btn-danger" style="margin-left: auto; margin-right: auto"><a href="?find_opponent=1">Find opponent</a></button>';
+?>
+<br>
+<span style="display: block">Attack specific user</span>
+<input type="text" name="user" id="name"/>
+<button type="button" class="btn btn-danger">Attack</button>
+
