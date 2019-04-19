22877f6cbf506f3237de77be270ad9a653e6e772
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-04-19 17:54:45 +0300

    insert result of result

--- project/system/Attack.php
+++ project/system/Attack.php
@@ -10,7 +10,7 @@ class Attack
         $user2_id = $defender;
         $user1_army = 0;
         $user2_army = 0;
-
+        $start = time();
         $user1_id_army = mysqli_query($dbc, "SELECT * FROM `user_army` WHERE `user_id` = " . $user1_id);
         while ($user1_id_army_r = mysqli_fetch_assoc($user1_id_army)) {
             $money = mysqli_query($dbc, "SELECT money FROM `army` WHERE `army_id` = " . $user1_id_army_r['army_name']);
@@ -24,10 +24,13 @@ class Attack
             $money = mysqli_fetch_assoc($money)['money'];
             $user2_army += $money * $user2_id_army_r['count'];
         }
+        $end = time();
         //ToDo insert into table battle result from batle
         if ($user1_army > $user2_army) {
+            mysqli_query($dbc, "INSERT INTO battle (attacker, defender, result, start, end) VALUES ('" . $user1_id . "', '" . $user2_id . "', 1, " . $start . ", " . $end . ")");
             $this->showAlert(1);
-        } elseif ($user1_army < $user2_army) {
+
+        } elseif ($user1_army <= $user2_army) {
             $this->showAlert(2);
         }
 
