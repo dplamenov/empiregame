75a342b22a47bdd3e965280e265f30a5efe07056
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-04-20 14:30:48 +0300

    user attack

--- project/attack.php
+++ project/attack.php
@@ -1,2 +1,9 @@
 <?php
-echo $_POST['user'];
+session_start();
+require 'config.php';
+$attack = new \System\Attack();
+$defender = mysqli_query($dbc,  "SELECT * FROM users WHERE user_name = '" . $_POST['user'] . "'");
+$defender = mysqli_fetch_assoc($defender);
+$defender = $defender['user_id'];
+$attack->startAttack($dbc, intval($_SESSION['user']['user_id']),
+    intval($defender));
