99d990ff523c31a640b96587f4646df44c1cd6d3
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-04-20 13:55:16 +0300

    bug fix

--- project/castle.php
+++ project/castle.php
@@ -20,18 +20,19 @@ if(mysqli_num_rows($your_battle) > 0){
     echo '<tr><th>Attacker</th><th>Defender</th><th>Result</th><th>Info</th></tr>';
     while($battle = mysqli_fetch_assoc($your_battle)){
         $attacker = mysqli_query($dbc, "SELECT * FROM users where user_id = " . $battle['attacker']);
-        $attacker = mysqli_fetch_assoc($attacker)['user_name'];
+        $attacker = mysqli_fetch_assoc($attacker);
         $defender = mysqli_query($dbc, "SELECT * FROM users where user_id = " . $battle['defender']);
-        $defender = mysqli_fetch_assoc($defender)['user_name'];
-        switch ($battle['result']){
-            case 1:
-                $result = 'Win';
-                break;
-            case 2:
-                $result = 'Lose';
-                break;
+        $defender = mysqli_fetch_assoc($defender);
+        if($battle['result'] == 1 && $attacker['user_id'] == $_SESSION['user']['user_id']){
+            $result = 'Win';
+        }elseif ($battle['result'] == 2 && $attacker['user_id'] == $_SESSION['user']['user_id']){
+            $result = 'Lose';
+        }elseif ($battle['result'] == 1 && $defender['user_id'] == $_SESSION['user']['user_id']){
+            $result = 'Lose';
+        }elseif ($battle['result'] == 2 && $defender['user_id'] == $_SESSION['user']['user_id']){
+            $result = 'Win';
         }
-        echo '<tr><td>'.$attacker.'</td><td>'.$defender.'</td><td>'.$result.'</td></tr>';
+        echo '<tr><td>'.$attacker['user_name'].'</td><td>'.$defender['user_name'].'</td><td>'.$result.'</td></tr>';
     }
 
     echo '</table>';
