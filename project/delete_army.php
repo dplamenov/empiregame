f9682ad5bcbd9f371cc91420f7e820d26a32ea90
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-04-26 15:11:34 +0300

    calc price of army

--- project/delete_army.php
+++ project/delete_army.php
@@ -1,2 +1,10 @@
 <?php
-echo $_POST['army'];
+session_start();
+include 'config.php';
+$army_id = $_POST['army'];
+$army = mysqli_query($dbc, "SELECT * FROM user_army WHERE army_id = " . $army_id);
+$army = mysqli_fetch_assoc($army);
+$price = mysqli_query($dbc, "SELECT * FROM army WHERE army_id = " . $army['army_name']);
+$price = mysqli_fetch_assoc($price)['money'];
+$total_price = $price * $army['count'];
+echo $total_price;
