60fa60626374afa7e7e6371785cca8e344582c57
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-04-27 16:29:17 +0300

    upgrade army

--- project/upgrade_army.php
+++ project/upgrade_army.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 include 'config.php';
-
 $user_army = mysqli_query($dbc, "SELECT * FROM user_army WHERE army_id = " . $_POST['army']);
 $user_army = mysqli_fetch_assoc($user_army);
 $army = mysqli_query($dbc, "SELECT * FROM army where army_id = " . $user_army['army_name']);
