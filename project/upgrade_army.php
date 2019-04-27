5f8303db2801682cf8980ee37f246021666aee23
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-04-27 16:20:55 +0300

    upgrade army

--- project/upgrade_army.php
+++ project/upgrade_army.php
@@ -1,4 +1,11 @@
 <?php
 session_start();
 include 'config.php';
-$army = mysqli_query($dbc, "SELECT * FROM user_army WHERE army_id = " . $army_id);
+$user_army = mysqli_query($dbc, "SELECT * FROM user_army WHERE army_id = " . $army_id);
+$user_army = mysqli_fetch_assoc($user_army);
+$army = mysqli_query($dbc, "SELECT * FROM army where army_id = " . $user_army['army_name']);
+$army = mysqli_fetch_assoc($army);
+$start = time();
+$end = $start + ($user_army['count'] * $army['time'] * 60);
+$level = $user_army['lvl'] + 1;
+mysqli_query($dbc, "INSERT INTO upgrade_army (army_name, start, end, level) VALUES (".$user_army['army_name'].", ".$start.", ".$end.", ".$level.")");
