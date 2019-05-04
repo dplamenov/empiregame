e038989ef34dd491b6402f8ffca45f59e773f7da
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-05-04 16:33:54 +0300

    bug fix

--- project/upgrade_army.php
+++ project/upgrade_army.php
@@ -8,4 +8,4 @@ $army = mysqli_fetch_assoc($army);
 $start = time();
 $end = $start + ($user_army['count'] * $army['time'] * 60);
 $level = $user_army['lvl'] + 1;
-mysqli_query($dbc, "INSERT INTO upgrade_army (army_name, start, end, level, user_id) VALUES (".$user_army['army_name'].", ".$start.", ".$end.", ".$level.", ".$_SESSION['user']['user_id'].")");
+mysqli_query($dbc, "INSERT INTO upgrade_army (army_id, army_name, start, end, level, user_id) VALUES (".$_POST['army'].",".$user_army['army_name'].", ".$start.", ".$end.", ".$level.", ".$_SESSION['user']['user_id'].")");
