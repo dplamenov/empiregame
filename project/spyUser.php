32c5a03793c9d384097f575c1084c123c9d7a5e6
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-05-16 19:39:02 +0300

    spy result

--- project/spyUser.php
+++ project/spyUser.php
@@ -5,7 +5,9 @@ $username = mysqli_real_escape_string($dbc, trim($_POST['username']));
 $user = mysqli_query($dbc, "SELECT * FROM users where user_name = '" . $username . "'");
 $user = mysqli_fetch_assoc($user);
 $army = mysqli_query($dbc, "SELECT * FROM user_army LEFT JOIN army ON user_army.army_id = army.army_id WHERE user_army.user_id = " . $user['user_id']);
+$army_count = 0;
 while($army_ = mysqli_fetch_assoc($army)){
-    var_dump($army_);
+    $army_count += $army_['count'];
 }
-echo $user['user_name'];
+?>
+<p>Username</p>
