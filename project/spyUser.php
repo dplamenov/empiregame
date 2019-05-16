c38e7e7543254c725e82506258e84645318c4648
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-05-16 19:35:41 +0300

    battle module

--- project/spyUser.php
+++ project/spyUser.php
@@ -4,3 +4,8 @@ include 'config.php';
 $username = mysqli_real_escape_string($dbc, trim($_POST['username']));
 $user = mysqli_query($dbc, "SELECT * FROM users where user_name = '" . $username . "'");
 $user = mysqli_fetch_assoc($user);
+$army = mysqli_query($dbc, "SELECT * FROM user_army LEFT JOIN army ON user_army.army_id = army.army_id WHERE user_army.user_id = " . $user['user_id']);
+while($army_ = mysqli_fetch_assoc($army)){
+    var_dump($army_);
+}
+echo $user['user_name'];
