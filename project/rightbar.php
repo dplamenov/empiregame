6a3c2cc03b09b0e96899ac6b025ab2fd1369b461
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-05-18 09:50:55 +0300

    statistics rightbar

--- project/rightbar.php
+++ project/rightbar.php
@@ -6,3 +6,5 @@ $level = mysqli_fetch_assoc($level)['level_id'];
 echo '<p>Your  money: $' . userdata($_SESSION['user']['user_id'], 'money', $dbc) . '</p>';
 echo '<span style="display: block">Level '.$level.' / XP: ' . userdata($_SESSION['user']['user_id'], 'xp', $dbc) . 'xp</span>';
 echo '<span>Population: ' . userdata($_SESSION['user']['user_id'], 'people', $dbc) . '</span><br>';
+
+echo '<a href="statistics.php"><img src="images/statistic-icon.png" title="Statistics"/><a/>';
