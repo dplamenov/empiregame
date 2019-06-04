4f407bdd1b1932448249180240dfedaaa645c378
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-06-04 18:10:36 +0300

    UserDTO

--- project/system/DTO/UserDTO.php
+++ project/system/DTO/UserDTO.php
@@ -5,5 +5,21 @@ namespace system\DTO;
 
 class UserDTO
 {
+    private $user_id;
 
+    private $user_name;
+
+    private $real_name;
+
+    private $email;
+
+    private $pass;
+
+    private $money;
+
+    private $xp;
+
+    private $people;
+
+    private $lastlogin;
 }
