79ac946e37c74692d84597ed73046ee8051e5635
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-06-04 18:12:08 +0300

    add getters and setters to userDTO

--- project/system/DTO/UserDTO.php
+++ project/system/DTO/UserDTO.php
@@ -22,4 +22,150 @@ class UserDTO
     private $people;
 
     private $lastlogin;
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * @param mixed $user_id
+     */
+    public function setUserId($user_id): void
+    {
+        $this->user_id = $user_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserName()
+    {
+        return $this->user_name;
+    }
+
+    /**
+     * @param mixed $user_name
+     */
+    public function setUserName($user_name): void
+    {
+        $this->user_name = $user_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRealName()
+    {
+        return $this->real_name;
+    }
+
+    /**
+     * @param mixed $real_name
+     */
+    public function setRealName($real_name): void
+    {
+        $this->real_name = $real_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPass()
+    {
+        return $this->pass;
+    }
+
+    /**
+     * @param mixed $pass
+     */
+    public function setPass($pass): void
+    {
+        $this->pass = $pass;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMoney()
+    {
+        return $this->money;
+    }
+
+    /**
+     * @param mixed $money
+     */
+    public function setMoney($money): void
+    {
+        $this->money = $money;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getXp()
+    {
+        return $this->xp;
+    }
+
+    /**
+     * @param mixed $xp
+     */
+    public function setXp($xp): void
+    {
+        $this->xp = $xp;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPeople()
+    {
+        return $this->people;
+    }
+
+    /**
+     * @param mixed $people
+     */
+    public function setPeople($people): void
+    {
+        $this->people = $people;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastlogin()
+    {
+        return $this->lastlogin;
+    }
+
+    /**
+     * @param mixed $lastlogin
+     */
+    public function setLastlogin($lastlogin): void
+    {
+        $this->lastlogin = $lastlogin;
+    }
+
+
 }
