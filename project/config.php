c116caf4209a0a44904293e7bfc9c1ffe1363e69
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-06-04 16:51:51 +0300

    code refactor

--- project/config.php
+++ project/config.php
@@ -32,30 +32,30 @@ $database = new PDODatabase(new PDO($dsn, $db_username, $db_password));
 mysqli_set_charset($dbc, 'utf8');
 function deletebuilding($dbc)
 {
-    $now_unix = time();
-    $sql = "SELECT * FROM `building_now` WHERE `end_time` <'" . $now_unix . "'";
-    $get_finished = mysqli_query($dbc, $sql);
-    $get_finish_builing_n = mysqli_num_rows($get_finished);
-    if ($get_finish_builing_n >= 1) {
-        while ($x = mysqli_fetch_assoc($get_finished)) {
+    $time = time();
+    $sql = "SELECT * FROM `building_now` WHERE `end_time` <'" . $time . "'";
+    $finish_building = mysqli_query($dbc, $sql);
+    $building_count = mysqli_num_rows($finish_building);
+    if ($building_count >= 1) {
+        while ($build = mysqli_fetch_assoc($finish_building)) {
             $get_build_id = "SELECT * FROM building_now WHERE user_id='" . $_SESSION['user']['user_id'] . "'";
 
             $get_build_id = mysqli_query($dbc, $get_build_id);
-            $r2a = mysqli_fetch_assoc($get_build_id);
-            $building_id = $x['building_name'];
-            $userid = $x['user_id'];
+            $build_ = mysqli_fetch_assoc($get_build_id);
+            $building_id = $build['building_name'];
+            $userid = $build['user_id'];
             $select_build_sql = "SELECT * FROM building WHERE building_id ='" . $building_id . "'";
             $select_build_q = mysqli_query($dbc, $select_build_sql);
             $select_build_a = mysqli_fetch_assoc($select_build_q);
             $givexp = $select_build_a['give_xp'];
             mysqli_query($dbc, "UPDATE users SET xp  = xp + '" . $givexp . "' WHERE user_id='" . $userid . "'");
-            $countub = mysqli_query($dbc, "SELECT * FROM users_building WHERE user_id='" . $_SESSION['user']['user_id'] . "' AND  building_id='" . $r2a['building_name'] . "'");
+            $countub = mysqli_query($dbc, "SELECT * FROM users_building WHERE user_id='" . $_SESSION['user']['user_id'] . "' AND  building_id='" . $build_['building_name'] . "'");
             if (mysqli_num_rows($countub) == 0) {
-                $select_build_lv = "INSERT INTO users_building (user_id,building_id,build_lv) VALUES ('" . $_SESSION['user']['user_id'] . "','" . $r2a['building_name'] . "','1')";
+                $select_build_lv = "INSERT INTO users_building (user_id,building_id,build_lv) VALUES ('" . $_SESSION['user']['user_id'] . "','" . $build_['building_name'] . "','1')";
 
                 mysqli_query($dbc, $select_build_lv);
             } else {
-                $lv = mysqli_query($dbc, "SELECT build_lv FROM users_building WHERE user_id='" . $_SESSION['user']['user_id'] . "' AND  building_id='" . $r2a['building_name'] . "'");
+                $lv = mysqli_query($dbc, "SELECT build_lv FROM users_building WHERE user_id='" . $_SESSION['user']['user_id'] . "' AND  building_id='" . $build_['building_name'] . "'");
                 $lva = mysqli_fetch_assoc($lv);
                 $lva['build_lv'];
                 $sql_update_u_l = "UPDATE users_building SET build_lv  = build_lv +'1' WHERE user_id='" . $userid . "'";
@@ -63,7 +63,7 @@ function deletebuilding($dbc)
                 $lv = mysqli_query($dbc, $sql_update_u_l);
             }
         }
-        $sql_delete = "DELETE FROM building_now WHERE `end_time` < '" . $now_unix . "' and `user_id` = " . $_SESSION['user']['user_id'];
+        $sql_delete = "DELETE FROM building_now WHERE `end_time` < '" . $time . "' and `user_id` = " . $_SESSION['user']['user_id'];
 
         mysqli_query($dbc, $sql_delete);
     }
