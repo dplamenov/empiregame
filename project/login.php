d8b2ac89b5669f553016b92f4a0e9f27292bf5bc
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-06-05 18:48:47 +0300

    user service

--- project/login.php
+++ project/login.php
@@ -3,24 +3,21 @@ session_start();
 include 'config.php';
 $user_name = $_POST['username'];
 $user_pass = $_POST['pass'];
-$users = mysqli_query($dbc, "SELECT * FROM users WHERE pass='" . $user_pass . "' AND user_name='" . $user_name . "' AND active = 1");
-$row = mysqli_fetch_assoc($users);
+$userService = new \system\Service\UserService();
+$user = $userService->login($user_name, $user_pass);
+var_dump($user);
 
-$_SESSION['user']['user_name'] = $row['user_name'];
-$_SESSION['user']['user_id'] = $row['user_id'];
-$_SESSION['user']['xp'] = $row['xp'];
-$_SESSION['user']['money'] = $row['money'];
+$_SESSION['user']['user_name'] = $user->getUserName();
+$_SESSION['user']['user_id'] = $user->getUserId();
+$_SESSION['user']['xp'] = $user->getXp();
+$_SESSION['user']['money'] = $user->getMoney();
 
-$lastlogin = $row['lastlogin'];
+$lastlogin = $user->getLastlogin();
 $hours = abs(time() - $lastlogin) / 60 / 60;
-mysqli_query($dbc, "UPDATE `users` SET `lastlogin`=  " . time(). " WHERE `user_id` = " . $row['user_id']);
+mysqli_query($dbc, "UPDATE `users` SET `lastlogin`=  " . time(). " WHERE `user_id` = " . $user->getUserId());
 if($hours >= 1){
-    mysqli_query($dbc, "UPDATE `users` SET `money`=  `money` + " . $hours * 50 . " WHERE `user_id` = " . $row['user_id']);
+    mysqli_query($dbc, "UPDATE `users` SET `money`=  `money` + " . $hours * 50 . " WHERE `user_id` = " . $user->getUserId());
 }
-if (mysqli_num_rows($users) == 1) {
-    $_SESSION['islogged'] = true;
-    echo 'window.location.reload(true); ';
-    exit;
-} else {
-    echo 'error';
-}
+$_SESSION['islogged'] = true;
+echo 'window.location.reload(true); ';
+exit;
--- project/system/Service/UserService.php
+++ project/system/Service/UserService.php
@@ -3,7 +3,31 @@
 namespace system\Service;
 
 
+use system\DTO\UserDTO;
+use system\Repository\UserRepository;
+
 class UserService
 {
+    private $userRepository;
 
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function login($username, $password)
+    {
+        /**
+         * @var $user UserDTO
+         */
+        $user = $this->userRepository->check($username);
+        /**
+         * if (!password_verify($user->getPass(), $password)) {
+            throw new \Exception('Wrong username or password');
+        }
+
+         */
+
+        return $user;
+    }
 }
