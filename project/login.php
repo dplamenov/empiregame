c7c864473994009635e9955344e043252a0a58c1
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-06-05 19:13:41 +0300

    use PDO instead of mysqli

--- project/login.php
+++ project/login.php
@@ -3,17 +3,23 @@ session_start();
 include 'config.php';
 $user_name = $_POST['username'];
 $user_pass = $_POST['pass'];
-$userService = new \system\Service\UserService(new \system\Repository\UserRepository($database));
+$userRepository = new \system\Repository\UserRepository($database);
+$userService = new \system\Service\UserService($userRepository);
 $user = $userService->login($user_name, $user_pass);
 $_SESSION['user']['user_name'] = $user->getUserName();
 $_SESSION['user']['user_id'] = $user->getUserId();
 $_SESSION['user']['xp'] = $user->getXp();
 $_SESSION['user']['money'] = $user->getMoney();
 
-$lastlogin = $user->getLastlogin();
-$hours = abs(time() - $lastlogin) / 60 / 60;
-mysqli_query($dbc, "UPDATE `users` SET `lastlogin`=  " . time(). " WHERE `user_id` = " . $user->getUserId());
-if($hours >= 1){
+$hours = abs(time() - $user->getLastlogin()) / 60 / 60;
+
+$stm = $database->query('UPDATE `users` SET `lastlogin` = :lastlogin WHERE `user_id` = :user_id');
+$stm->execute([
+    'lastlogin' => time(),
+    'user_id' => $user->getUserId()
+]);
+
+if ($hours >= 1) {
     mysqli_query($dbc, "UPDATE `users` SET `money`=  `money` + " . $hours * 50 . " WHERE `user_id` = " . $user->getUserId());
 }
 $_SESSION['islogged'] = true;
