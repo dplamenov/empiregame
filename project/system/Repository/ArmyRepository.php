1da240a9e67adc33e2a2737b06bffe66c3bcd2b9
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-06-06 15:04:52 +0300

    create ArmyRepository

---
+++ project/system/Repository/ArmyRepository.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Project: empiregame.
+ * File: ArmyRepository.php
+ * Developer: dplamenov@icloud.com
+ * Date: 6.6.2019 г.
+ * Time: 15:04
+ */
+
+namespace system\Repository;
+
+
+class ArmyRepository
+{
+
+}
