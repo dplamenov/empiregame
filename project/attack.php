0e416be9832894c22ea4dc2c4aa728d80d8858a8
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-06-06 12:26:10 +0300

    attack.php

--- project/attack.php
+++ project/attack.php
@@ -5,10 +5,9 @@ $attack = new \System\Attack();
 $userRepository = new system\Repository\UserRepository($database);
 $userService = new \system\Service\UserService($userRepository);
 $defender = $userService->findByUsername($_POST['user']);
-$defender = $defender->getUserId();
 try {
     $attack->startAttack($dbc, intval($_SESSION['user']['user_id']),
-        intval($defender));
+        intval($defender->getUserId()));
 }catch (Exception $exception){
     $json['code'] = '1';
     $json['exception'] = $exception->getMessage();
