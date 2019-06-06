66a0f558708cc6d9c7ff84416529fdcccbe3429e
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-06-06 15:12:22 +0300

    ArmyRepository method allArmy

--- project/system/Repository/ArmyRepository.php
+++ project/system/Repository/ArmyRepository.php
@@ -3,7 +3,22 @@
 namespace system\Repository;
 
 
+use Database\PDODatabase;
+use system\DTO\ArmyDTO;
+
 class ArmyRepository
 {
+    private $database;
+    public function __construct(PDODatabase $PDODatabase)
+    {
+        $this->database = $PDODatabase;
+    }
 
+    public function allArmy()
+    {
+        $stm = $this->database->query('SELECT * FROM army');
+        $result = $stm->execute();
+
+        return $result->fetch(ArmyDTO::class);
+    }
 }
