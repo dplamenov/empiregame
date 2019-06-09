5a93af30c388c14d829d643cb1a6679cbbef5efe
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-06-09 07:48:27 +0300

    remove unused params from methods

--- project/auto_refresharmy.php
+++ project/auto_refresharmy.php
@@ -4,13 +4,13 @@ session_start();
 include_once "config.php";
 $armyRepository = new \system\Repository\ArmyRepository($database);
 $armyService = new \system\Service\ArmyService($armyRepository);
-function getCount(\Database\PDODatabase $database, $armyRepository, $armyService): int
+function getCount($armyRepository, $armyService): int
 {
     $army = $armyService->allArmy();
     return iterator_count($army);
 }
 
-function getArmyById(\Database\PDODatabase $database, int $user_id, int $army_id, \system\Service\armyService $armyService): string
+function getArmyById(int $user_id, int $army_id, \system\Service\armyService $armyService): string
 {
     $army = $armyService->trainingArmyByUser($user_id, $army_id);
     if (date("H:i:s", ($army->getEndTime() - time() - 7200)) == "00:00:00") {
@@ -20,9 +20,9 @@ function getArmyById(\Database\PDODatabase $database, int $user_id, int $army_id
 }
 
 if (@$_POST['getCount'] == true) {
-    echo getCount($database, $armyRepository, $armyService);
+    echo getCount($armyRepository, $armyService);
 }
 if (isset($_POST['id'])) {
     $id = trim($_POST['id']);
-    echo getArmyById($database, $_SESSION['user']['user_id'], $id, $armyService);
+    echo getArmyById($_SESSION['user']['user_id'], $id, $armyService);
 }
