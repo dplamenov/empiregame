443c7ea2ba56a4d5fa99bb9d51abf1d33ee6857c
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-06-09 07:35:43 +0300

    create BuildingService

---
+++ project/system/Service/BuildingService.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Project: empiregame.
+ * File: BuildingService.php
+ * Developer: dplamenov@icloud.com
+ * Date: 9.6.2019 г.
+ * Time: 7:35
+ */
+
+namespace system\Service;
+
+
+class BuildingService
+{
+
+}
